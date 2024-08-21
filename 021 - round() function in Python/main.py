2767ac6510d5520a4e6dd7af76bb5311b8fa6b13
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-22 00:14:31 +0530

    lec 21

---
+++ 021 - round() function in Python/main.py
@@ -0,0 +1,41 @@
+# round( number, no. of digits)
+
+print( round(21.2223, 2))
+print( round(21.2223))
+print( round(21.2563, 2))
+
+print( round(7))
+print( round(7, 2))
+print( round(7.61))
+print( round(2.6666, 2))
+print( round(2.6657, 0))
+
+print( round(7.5))
+print( round(6.5))
+# round off to nearest even integer
+
+print( round(11.5))
+print( round(12.5))
+
+print( round(678,2))
+print( round(678,0))
+
+print( round(678,-1))
+# if (-ve) -> nearest power of 10
+print( round(678,-2))
+print( round(678,-3))
+
+# if digit is greater then no. of round off(-ve) then 0
+print( round(678,-4))
+print( round(678,-5))
+print( round(678,-6))
+
+print( round(665,-1))
+print( round(675,-1))
+
+print( round(-8/3))
+print( round(-1.5))
+print( round(-8/3, 2))
+
+print( round(674.1012, -1))
+print( round(1212,-2))
