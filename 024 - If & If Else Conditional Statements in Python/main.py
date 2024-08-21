40cdc40690bce828c8b3301b997fc851ab1d143d
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-22 00:49:27 +0530

    lec 24

---
+++ 024 - If & If Else Conditional Statements in Python/main.py
@@ -0,0 +1,14 @@
+height = float(input("Enter height in feet : "))
+
+if( height > 3):
+    print("Hii")
+
+print("Out of if block")
+
+if( height > 3):
+    print("Buy Token")
+    print("Now you can board the metro!")
+else:
+    print("No Token reqired")
+
+print("Out of if else block")
