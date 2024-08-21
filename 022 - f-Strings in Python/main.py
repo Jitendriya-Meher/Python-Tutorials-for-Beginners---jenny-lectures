accde2a1cbd88eb39fb0ddce7632518384a826cd
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-22 00:24:43 +0530

    lec 22

---
+++ 022 - f-Strings in Python/main.py
@@ -0,0 +1,9 @@
+name = "Jitendriya"
+age = 22
+height = 1.8
+
+print("My name is "+name,"I am "+str(age),"years old")
+
+print(f"My name is {name} I am {age} years old. My height is {height} meters.")
+
+print(f" Age*2 = {age*2}")
