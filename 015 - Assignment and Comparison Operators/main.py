acab32d4d94bd7951c07ab7c30d41c6465db34bb
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-21 20:11:53 +0530

    lec 15

---
+++ 015 - Assignment and Comparison Operators/main.py
@@ -0,0 +1,24 @@
+a,b,c = 1,2,3
+print(a,b,c)
+
+# comparison operators
+print(a<1)
+print(a<=1)
+print(a>1)
+print(a>=1)
+print(a==1)
+print(a!=1)
+
+# assignment operators
+a += 5
+print("a :", a)
+a -= 5
+print("a :", a)
+a *= 5
+print("a :", a)
+a **= 3
+print("a :", a)
+a //= 2
+print("a :", a)
+a /= 5
+print("a :", a)
