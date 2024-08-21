151e1669857702bdefa21964ecebbe5d79afa368
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-21 19:29:05 +0530

    lec 11 , 1

---
+++ 011 - Data Types in Python/main.py
@@ -0,0 +1,45 @@
+# binary
+print(0b10)
+print(0B1010)
+print(type(0B1010))
+
+# octal
+print(0o10)
+print(0O100)
+
+# hexa decimal
+print(0x12)
+print(0X100)
+
+var_1 = 1234123412341234124123456789
+print(var_1)
+print(type(var_1))
+
+var_2 = 12.34123412341234124123456789
+print(var_2)
+print(type(var_2))
+
+var_3 = 0x100
+print(var_3)
+print(type(var_3))
+
+name = "Jitendriya"
+print(name)
+print(type(name))
+print(name[2])
+
+name_1 = "Jitendriya"
+name_2 = "Meher"
+a=1
+
+print("123"+name_1)
+print(name_1+name_2)
+# unsupported operand type(s) for +: 'int' and 'str'
+# print(a+name_1)
+
+# escapr character
+print(5*"Jenny\'s \\n \"Lecture\"\n")
+
+var= True
+print(var)
+print(type(var))
---
+++ 012 - Type Checking, Type Error and Type Conversion/main.py
@@ -0,0 +1,21 @@
+length = len("Jitendriya")
+print("Your name has " + str(length) + " characters")
+
+new_length = str(length)
+print(type(new_length))
+
+print("10" + "10")
+print(int("10") + int("10"))
+
+print(10 + float("20"))
+
+name = "123"
+# name = "jiten"
+print(10 + int(name))
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+sum = a+b
+
+print("sum =",sum)
