6f60a271a88e37e07341bb2f3a615cc52d7d3998
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-21 19:56:41 +0530

    lec 14

---
+++ 014 - Arithmetic Operators/hw.py
@@ -0,0 +1,6 @@
+weight = float(input("Enter your weight in Kg : "))
+height = float(input("Enter your height in meters : "))
+
+bmi = weight / (height**2)
+
+print("BMI : " , bmi)
---
+++ 014 - Arithmetic Operators/main.py
@@ -0,0 +1,14 @@
+# precidence and associativity
+# PEMDAS
+# 1 - (,)
+# 2 - ** - R-L
+# 3 - *,/,//,% - L-R
+# 4 - +,- - L-R
+
+print(5/2)
+print(5//2)
+print(5**2)
+print(5*2)
+print(5%2)
+print(5+2)
+print(5-2)
