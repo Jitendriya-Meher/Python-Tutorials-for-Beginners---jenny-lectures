84c63dace2cd7b098c75369bd10ced403ab22144
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-21 20:19:06 +0530

    lec 16

---
+++ 016 - Logical Operators/main.py
@@ -0,0 +1,13 @@
+a,b = 5,4
+
+print(a>4 and b<10)
+print(a>10 and b<10)
+print(a>10 and b>10)
+
+print(a>4 or b<10)
+print(a>10 or b<10)
+print(a>10 or b>10)
+
+a = False
+print("a:",a)
+print("not a:",not(a))
