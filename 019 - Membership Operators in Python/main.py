14e418dbb56b9e83099229597c5b8e17da81c257
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-21 23:42:26 +0530

    lec 19

---
+++ 019 - Membership Operators in Python/main.py
@@ -0,0 +1,19 @@
+str = "Jitendriya"
+
+print('y' in str)
+print('ten' in str)
+print('j' in str)
+
+print('j' not in str)
+print('ten' not in str)
+
+print()
+
+l = [1,2,3,4,5,6,7,8,9]
+
+print(1 in l)
+print(11 in l)
+print([1,2,3] in l)
+
+print(1 not in l)
+print(11 not in l)
