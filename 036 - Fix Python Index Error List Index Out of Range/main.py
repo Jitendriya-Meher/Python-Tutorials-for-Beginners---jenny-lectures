985c713eb27bd5f9d8bdefc0a8ea16174b110698
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-22 14:28:41 +0530

    lec 36

---
+++ 036 - Fix Python Index Error List Index Out of Range/main.py
@@ -0,0 +1,12 @@
+
+names = ['a', 'b', 'c', 'd', 'e', 'f']
+
+print(f"hi, {names[3]}")
+# print(f"hi, {names[6]}")
+
+length = len(names)
+print(length)
+
+print(f"hi, {names[length-1]}")
+print(f"hi, {names[-1]}")
+# print(f"hi, {names[-7]}")
