64db1c230fa8f2b2210f0ec22b9f2ce66c405b7f
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-22 23:31:33 +0530

    lec 43

---
+++ 043 - For else in Python/main.py
@@ -0,0 +1,14 @@
+t = (1,2,3,4,5)
+
+for i in t:
+    print(i)
+else:
+    print("loop sucessfully completed")
+    
+
+for i in t:
+    print(i)
+    if( i==2):
+        break
+else:
+    print("loop sucessfully completed")
