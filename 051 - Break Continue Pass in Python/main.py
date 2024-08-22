91f725864c0e9ab33d7a4a0b17a4e132bbc1b73b
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-23 01:07:58 +0530

    lec 51

---
+++ 051 - Break Continue Pass in Python/main.py
@@ -0,0 +1,37 @@
+i=1
+
+while( i<=10):
+    print(i,end=" ")
+    i+=1
+    if( i==7):
+        break
+    print("Hii")
+
+print("\nend loop")
+
+
+i=1
+
+while( i<=10):
+    print(i,end=" ")
+    i+=1
+    if( i==7):
+        continue
+    print("Hii")
+
+print("\nend loop")
+
+
+for i in range (10):
+    pass
+
+i=1
+
+while( i<=10):
+    print(i,end=" ")
+    i+=1
+    if( i==7):
+        pass
+    print("Hii")
+
+print("\nend loop")
