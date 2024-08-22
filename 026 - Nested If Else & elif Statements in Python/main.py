d62a12cf31af6e72dd8ad8efab64ded53187968d
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-22 10:41:13 +0530

    lec 26

---
+++ 026 - Nested If Else & elif Statements in Python/main.py
@@ -0,0 +1,14 @@
+a = 30
+
+if a%2 == 0:
+    print("number is even")
+    if( a > 30):
+        print("number is greater than 30")
+    elif( a < 30 ):
+        print("number is less than to 30")
+    else:
+        print("number is 30")
+else:
+    print("number is odd")
+
+print("nested if else  bye..")
