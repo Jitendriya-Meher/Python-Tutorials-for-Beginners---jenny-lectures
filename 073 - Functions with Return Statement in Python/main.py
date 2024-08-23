a711f40d297ebb8c8018c2e615d688ffcfa60e57
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-24 00:35:47 +0530

    lec 73

---
+++ 073 - Functions with Return Statement in Python/main.py
@@ -0,0 +1,14 @@
+def add (a,b):
+    c = a+b
+    # return
+    return c
+
+# return
+
+print(add(1,2))
+print(add(2,3))
+
+def change( fname, lname):
+    return f"{fname.title()} {lname.title()}"
+
+print(change("amit","meheR"))
