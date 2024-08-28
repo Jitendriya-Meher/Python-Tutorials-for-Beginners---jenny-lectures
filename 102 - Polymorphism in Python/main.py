90820d1b7785fa2204121b5f4c4009c315afd96f
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-29 02:13:16 +0530

    lec 102

---
+++ 102 - Polymorphism in Python/main.py
@@ -0,0 +1,4 @@
+# Polymorphism is a core concept in object-oriented programming (OOP) that allows objects of different classes to be treated as objects of a common superclass. It enables a single interface to be used for a general class of actions, with the specific action determined by the exact nature of the situation.
+
+print(1+2)
+print("1"+"2")
