b921f0d07e1c2b100ceabe4670a0667641117562
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-28 22:04:25 +0530

    lec 094

---
+++ 094 - Diamond Problem in Inheritance/main.py
@@ -0,0 +1,21 @@
+class A:
+    def show(self):
+        print("show A")
+
+class B(A):
+    def show(self):
+        print("show B")
+
+class C(A):
+    def show(self):
+        print("show C")
+
+class D(B,C):
+    def show(self):
+        print("show D")
+
+d = D()
+d.show()
+
+print(D.mro())
+print(D.__mro__)
