d8b7f57bf370c633a5b163cd7c26ffdbfd8eeb1d
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-08-28 22:39:17 +0530

    lec 098

---
+++ 098 - Abstraction in Python/main.py
@@ -0,0 +1,15 @@
+
+# Abstraction in Python is a core concept in object-oriented programming (OOP) that allows you to hide the internal implementation details of a class and expose only the necessary and relevant parts.
+
+# Abstract Classes:
+
+# Python uses abstract classes to achieve abstraction.
+# An abstract class is a class that cannot be instantiated and is typically used as a blueprint for other classes.
+# Abstract classes in Python are created using the abc (Abstract Base Classes) module.
+# An abstract class can have one or more abstract methods, which are methods declared but contain no implementation.
+
+
+# Abstract Methods:
+
+# An abstract method is a method that is declared in the abstract class but doesn't have any implementation in it. The implementation is expected to be provided by the subclasses.
+# Any class inheriting an abstract class must implement all the abstract methods, or it will also be considered abstract and cannot be instantiated.
